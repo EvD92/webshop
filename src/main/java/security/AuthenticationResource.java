9ecf4bb95e035533bd869d70ab57585f3693db65
EvD92 <edo.v.draanen@hotmail.com>
2018-03-20 20:27:13 +0100

    .

--- src/main/java/security/AuthenticationResource.java
+++ src/main/java/security/AuthenticationResource.java
@@ -3,12 +3,16 @@ package security;
 import java.security.Key;
 import java.util.Calendar;
 
+import javax.annotation.security.RolesAllowed;
 import javax.json.Json;
 import javax.json.JsonObjectBuilder;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -34,7 +38,7 @@ public class AuthenticationResource {
 			if (role == null) {
 				throw new IllegalArgumentException("No user found!");
 			}
-			Klant k = dao.getKlant(email);
+//			Klant k = dao.getKlant(email);
 			// Issue a token for the user
 			Calendar expiration = Calendar.getInstance();
 			expiration.add(Calendar.MINUTE, 30);
@@ -43,14 +47,43 @@ public class AuthenticationResource {
 			// Return the token on the response
 			JsonObjectBuilder job = Json.createObjectBuilder();
 			job.add("token", token);
-			job.add("naam", k.getNaam());
-			job.add("straat", k.getAdres().getStraat());
-			job.add("straatnummer", k.getAdres().getStraatNummer());
+//			job.add("naam", k.getNaam());
+//			job.add("straat", k.getAdres().getStraat());
+//			job.add("straatnummer", k.getAdres().getStraatNummer());
 			
 			
 			return Response.ok(job.build().toString()).build();
 		} catch (JwtException | IllegalArgumentException e) {
 			return Response.status(Response.Status.UNAUTHORIZED).build();
 		}
+	}
+	
+	@GET
+	@Path("{email}")
+//	@RolesAllowed("guest")
+	@Produces("application/json")	
+	public Response getUser(@PathParam("email") String email){		
+		System.out.println(email);
+		try {
+			// Authenticate the user against the database
+			UserDao dao = new UserDao();
+			Klant k = dao.getKlant(email);
+			if (k.getNaam() == null) {
+				throw new IllegalArgumentException("No user found!");
+			}
+			JsonObjectBuilder job = Json.createObjectBuilder();
+//			job.add("token", token);
+			job.add("naam", k.getNaam());
+			System.out.println(k.getNaam() + "naam");
+			job.add("straat", k.getAdres().getStraat());
+			System.out.println(k.getAdres().getStraat());
+			job.add("straatnummer", k.getAdres().getStraatNummer());
+			
+			
+			return Response.ok(job.build().toString()).build();
+		} catch (JwtException | IllegalArgumentException e) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+		
 	}
 }
--- src/main/java/security/UserDao.java
+++ src/main/java/security/UserDao.java
@@ -31,15 +31,16 @@ public class UserDao extends security.BaseDao {
 		String role = null;
 		// String query = "SELECT role FROM lid WHERE email = ? AND password =
 		// ?";
-		String query = "SELECT NAAM FROM CATEGORIE WHERE CATEGORIE_ID = 1";
+//		String query = "SELECT NAAM FROM CATEGORIE WHERE CATEGORIE_ID = 1";
+		String query = "select ROLE from klant where EMAIL = ? and PASSWORD = ?";
 		try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS)) {
 			PreparedStatement pstmt = con.prepareStatement(query);
-			// pstmt.setString(1, email);
-			// pstmt.setString(2, password);
+			 pstmt.setString(1, email);
+			 pstmt.setString(2, password);
 			ResultSet rs = pstmt.executeQuery();
 			if (rs.next())
 				// role = rs.getString("role");
-				role = rs.getString("NAAM");
+				role = rs.getString("ROLE");
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		}
@@ -49,13 +50,14 @@ public class UserDao extends security.BaseDao {
 	public Klant getKlant(String email) {
 		Klant k = new Klant();
 		Adres a = new Adres();
+		System.out.println(email + "getklantemail");
 		String query = "select k.naam, a.straat, a.straatnummer from adres a, klant k where k.klant_id = a.klant_id and email = ?";
 		try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS)) {
 			PreparedStatement pstmt = con.prepareStatement(query);
 			pstmt.setString(1, email);
 			// pstmt.setString(2, password);
 			ResultSet rs = pstmt.executeQuery();
-			if (rs.next())
+			while (rs.next())
 				k.setNaam(rs.getString("NAAM"));
 				a.setStraat(rs.getString("STRAAT"));
 				a.setStraatNummer(rs.getInt("STRAATNUMMER"));
